59b134dc2b9c8e979aeaa9ae331fdc1a4daee2cd
sjgresha <sbjg2545@gmail.com>
2023-07-12 23:00:24 -0700

    Worked on js, added more function to get the program running better, adjusted the class and id of html elements to be called upon easier,add temp questions to quiz

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,144 @@
-var startButton = document.getElementById('start');
-var quiz = document.getElementById('quiz');
-var questions = document.getElementById('questions');
-var answerBtns = document.querySelector('btn');
+//These are just sample questions for now. Will write real ones soon
+var quizData = [
+  {
+    question: "Who am I?",
+    answers: ["I am you", "You are me", "You are you"],
+    correctAnswer: 2
+  },
+  {
+    question: "Where are we",
+    answers: ["We are here", "We are there", "We are everywhere", "We are no where"],
+    correctAnswer: 3
+  },
+  {
+    question: "What am I doing?",
+    answers: ["You are doing everything", "You are doing nothing", "You are coding"],
+    correctAnswer: 2
+  },
+  {
+    question: "When am I",
+    answers: ["You are now", "You were then", "you will be then", "you never were"],
+    correctAnswer: 0
+  },
+];
+
+var currentQuestion = 0;
+var score = 0;
+var timeRemaining = 60;
+var timerInterval;
+
+var startButton = document.getElementById('startButton');
+var quizGame = document.getElementById('quiz-game');
+var questionEl = document.getElementById('question');
+var answersEl = document.getElementById('answers');
 var timerEl = document.getElementById('timer');
-var countdown = 60;
-var score;
-var wrongAnswers;
+var resultContainer = document.getElementById('result-container');
+var scoreEl = document.getElementById('score');
+var initialsInput = document.getElementById('initials');
+var submitScoreButton = document.getElementById('submitScore');
+var highscoreLink = document.getElementById('highscore');
+var highscoreContainer = document.getElementById('highscore-container');
+var highscoreList = document.getElementById('highscore-list');
 
-startButton.addEventListener('click', function() {
-    quiz.style.display = 'block';
-    startButton.style.display = 'none';
-    var countdownScore = setInterval(function() {
-        countdown--;
-        timerEl.textContent = countdown;
+function startQuiz() {
+  startButton.style.display = 'none';
+  quizGame.style.display = 'block';
 
-        if (countdown === 0) {
-            clearInterval(countdownScore);
-            alert('Times up');
-        }
-    }, 1000);
+  showQuestion();
+  startTimer();
+}
 
-    answerBtns.addEventListener('click', function() {
-        questions.textContent = 'Question 2 goes here';
-    })
-});
+function showQuestion() {
+  var quiz = quizData[currentQuestion];
+  questionEl.textContent = quiz.question;
+
+  answersEl.textContent = '';
+  quiz.answers.forEach((choice, index) => {
+    var button = document.createElement('button');
+    button.textContent = choice;
+    button.addEventListener('click', handleAnswerClick);
+    answersEl.appendChild(button);
+  });
+}
+
+function handleAnswerClick(event) {
+  var selectedChoice = event.target;
+  var selectedAnswer = selectedChoice.textContent;
+  var quiz = quizData[currentQuestion];
+
+  if (quiz.answers[quiz.correctAnswer] === selectedAnswer) {
+    score++;
+  } else {
+    timeRemaining -= 5;
+    if (timeRemaining < 0) {
+      timeRemaining = 0;
+    }
+  }
+
+  currentQuestion++;
+
+  if (currentQuestion < quizData.length) {
+    showQuestion();
+  } else {
+    endQuiz();
+  }
+}
+
+function endQuiz() {
+  clearInterval(timerInterval);
+  timerEl.textContent = timeRemaining;
+
+  quizGame.style.display = 'none';
+  resultContainer.style.display = 'block';
+  scoreEl.textContent = timeRemaining;
+
+  submitScoreButton.addEventListener('click', submitScore);
+  highscoreLink.addEventListener('click', showHighscores);
+}
+
+function submitScore() {
+  var initials = initialsInput.value.trim();
+  if (initials !== '') {
+    var highscoreItem = document.createElement('li');
+    highscoreItem.textContent = initials + ': ' + timeRemaining;
+    highscoreList.appendChild(highscoreItem);
+  }
+
+  initialsInput.value = '';
+  resultContainer.style.display = 'none';
+  highscoreContainer.style.display = 'block';
+}
+
+function showHighscores() {
+  quizGame.style.display = 'none';
+  resultContainer.style.display = 'none';
+  highscoreContainer.style.display = 'block';
+
+  displayHighscores();
+}
+
+function displayHighscores() {
+    highscoreList.textContent = "";
+    
+    var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    
+    highScores.forEach((entry) => {
+        var highscoreItem = document.createElement('li');
+        highscoreItem.textContent = entry.initials + ': ' + entry.score;
+        highscoreList.appendChild(highscoreItem);
+      });
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timeRemaining--;
+    timerEl.textContent = timeRemaining;
+
+    if (timeRemaining <= 0) {
+      clearInterval(timerInterval);
+      endQuiz();
+    }
+  }, 1000);
+}
+
+startButton.addEventListener('click', startQuiz);
